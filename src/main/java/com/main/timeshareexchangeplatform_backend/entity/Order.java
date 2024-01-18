efcf0967ced27389f1e5e6a520e832a63b261921
dofu18 <phu2003hk@gmail.com>
2024-01-18 16:46:34 +0700

    test

---
+++ src/main/java/com/main/timeshareexchangeplatform_backend/entity/Order.java
@@ -0,0 +1,18 @@
+package com.main.timeshareexchangeplatform_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name = "Orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+
+
+
+}
