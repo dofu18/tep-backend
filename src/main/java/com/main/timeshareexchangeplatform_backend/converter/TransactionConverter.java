6f31a13bf0eb3e2c94fdbc6ecff4634732c088fc
dofu18 <phu2003hk@gmail.com>
2024-02-29 16:36:12 +0700

    get transaction by user id

--- src/main/java/com/main/timeshareexchangeplatform_backend/converter/TransactionConverter.java
+++ src/main/java/com/main/timeshareexchangeplatform_backend/converter/TransactionConverter.java
@@ -1,7 +1,10 @@
 package com.main.timeshareexchangeplatform_backend.converter;
 
+import com.main.timeshareexchangeplatform_backend.dto.BookingResponse;
 import com.main.timeshareexchangeplatform_backend.dto.DestinationModel;
 import com.main.timeshareexchangeplatform_backend.dto.TransactionDTO;
+import com.main.timeshareexchangeplatform_backend.dto.TransactionResponse;
+import com.main.timeshareexchangeplatform_backend.entity.Booking;
 import com.main.timeshareexchangeplatform_backend.entity.Destination;
 import com.main.timeshareexchangeplatform_backend.entity.Transaction_history;
 import com.main.timeshareexchangeplatform_backend.repository.ServicePackRepository;
@@ -16,6 +19,10 @@ public class TransactionConverter {
     ServicePackRepository servicePackRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    ServicePackConverter servicePackConverter;
+    @Autowired
+    UserConverter userConverter;
     public Transaction_history toEntity(TransactionDTO dto) {
 
         Transaction_history entity = new Transaction_history();
@@ -42,6 +49,32 @@ public class TransactionConverter {
         dto.setTransaction_code(entity.getTransactionCode());
 
         return dto;
+    }
+
+    public TransactionResponse toRespone(Transaction_history entity) {
+        TransactionResponse dto = new TransactionResponse();
+
+        dto.setTransaction_id(entity.getTransaction_id());
+        dto.setTransaction_fee(entity.getTransaction_fee());
+        dto.setTransaction_time(entity.getTransaction_time());
+        dto.setService_id(servicePackConverter.toDTO(entity.getServicePack()));
+        dto.setUser_id(userConverter.toDTO(entity.getUser()));
+        dto.setTransaction_code(entity.getTransactionCode());
+
+        return dto;
+    }
+
+    public Transaction_history toResEntity(TransactionResponse dto) {
+        Transaction_history entity = new Transaction_history();
+
+        entity.setTransaction_id(dto.getTransaction_id());
+        entity.setTransaction_fee(dto.getTransaction_fee());
+        entity.setTransaction_time(dto.getTransaction_time());
+        entity.setServicePack(servicePackConverter.toEntity(dto.getService_id()));
+        entity.setUser(userConverter.toEntity(dto.getUser_id()));
+        entity.setTransactionCode(dto.getTransaction_code());
+
+        return entity;
     }
 
 }
